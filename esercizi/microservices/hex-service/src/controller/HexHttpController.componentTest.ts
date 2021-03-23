922511a8b77242cb9a993c073f01f14e4e024168
mscalfi <marco.scalfi@protonmail.com>
2021-03-23 16:24:01 +0100

    hex to hsl via api working

--- esercizi/microservices/hex-service/src/controller/HexHttpController.componentTest.ts
+++ esercizi/microservices/hex-service/src/controller/HexHttpController.componentTest.ts
@@ -1,6 +1,6 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import { hex2rgbTestData } from '../../../commons/src/test-data/colors';
+import { hex2hslTestData, hex2rgbTestData } from '../../../commons/src/test-data/colors';
 import * as config from '../../server-config.json'
 
 chai.config.includeStack = true;
@@ -20,6 +20,20 @@ describe('hex service REST API Test', () => {
                     should.not.exist(err);
                     res.should.have.status(200);
                     res.body.should.deep.equal(test.rgbValue);
+                    done();
+                });
+        });
+    });
+
+    hex2hslTestData.forEach((test) => {
+        it(`should convert via API ${JSON.stringify(test.hexValue)} to ${JSON.stringify(test.hslValue)}`, (done) => {
+            chai.request(url)
+                .get('/hex/toHSL')
+                .query(`color=${JSON.stringify(test.hexValue)}`)
+                .end((err, res) => {
+                    should.not.exist(err);
+                    res.should.have.status(200);
+                    res.body.should.deep.equal(test.hslValue);
                     done();
                 });
         });
--- esercizi/microservices/hex-service/src/controller/HexHttpController.ts
+++ esercizi/microservices/hex-service/src/controller/HexHttpController.ts
@@ -1,12 +1,19 @@
-import {hexToRGB} from '../service/HexService'; 
+import {hexToHSL, hexToRGB} from '../service/HexService'; 
 import {Express} from 'express';
-import { TtfHex, TtfRgb } from '../../../commons/src/model/Color';
+import { TtfHex, TtfHsl, TtfRgb } from '../../../commons/src/model/Color';
 
 class HexHttpController {
     constructor(server: Express) {
         server.get('/hex/toRGB', (req, res) => {
             const color = JSON.parse(req.query.color as string) as TtfHex;
             const convertedColor: TtfRgb = hexToRGB(color);
+
+            res.send(convertedColor);
+        });
+
+        server.get('/hex/toHSL', (req, res) => {
+            const color = JSON.parse(req.query.color as string) as TtfHex;
+            const convertedColor: TtfHsl = hexToHSL(color);
 
             res.send(convertedColor);
         });
